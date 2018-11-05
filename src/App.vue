bb36f609be3ee87153ef3997df5a197bf06aba86
SonJaerock <kadamon2007@naver.com>
2018-11-05 19:47:47 +0900

    init structure

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,37 @@
 <template>
-  <div id="app">
-    <img src="./assets/logo.png">
-    <router-view/>
+  <div class="container">
+    <div class="app">
+      <app-problem></app-problem>
+      <app-result></app-result>
+      <app-footer></app-footer>
+    </div>
   </div>
 </template>
 
 <script>
+import Problem from './components/Problem.vue';
+import Result from './components/Result.vue';
+import Footer from './components/Footer.vue';
+
 export default {
-  name: 'App'
+  name: 'App',
+  components: {
+    'app-problem' : Problem,
+    'app-result' : Result,
+    'app-footer' : Footer
+  }
 }
 </script>
 
 <style>
-#app {
+.container {
+  width:100%;
+}
+
+.app {
+  width: 1024px;
+  margin-left: auto;
+  margin-right: auto;
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
