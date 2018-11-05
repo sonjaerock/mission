4df0912e18e740193e047efaeef482da7c58300e
SonJaerock <kadamon2007@naver.com>
2018-11-05 20:32:18 +0900

    add list skeleton

--- src/components/Problem.vue
+++ src/components/Problem.vue
@@ -1,6 +1,26 @@
 <template>
-  <div class="problem">
-      {{ problemList }}
+  <div class="problems">
+    <div class="problem" v-for="(problem, index) in problemList">
+      <span class="title">{{ index+1 }}. {{ problem.problem_text }}</span>
+      <div
+        v-if="problem.type === 1"
+        class="answer">
+        <span
+          v-for="choice in parseChoice(problem.choices)"
+          class="">
+          <input
+            type="radio"
+            name=""
+            :value="choice">
+            {{ choice }}
+        </span>
+      </div>
+      <div
+        v-else
+        class="answer">
+        <textarea name="name" rows="8" cols="80"></textarea>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -11,7 +31,7 @@ export default {
   name: 'Problem',
   data () {
     return {
-      
+
     }
   },
   computed: {
@@ -24,6 +44,9 @@ export default {
     setProblems: function(){
       console.log("ddd");
       this.$store.dispatch("setProblemList");
+    },
+    parseChoice: function(val){
+      return JSON.parse(val);
     }
   },
   created() {
@@ -34,5 +57,13 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-
+  * {
+    text-align: left;
+  }
+  .problem {
+    margin-bottom: 30px;
+  }
+  .answer {
+    text-align: right;
+  }
 </style>
